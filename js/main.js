c1af769f9a280749a3b4eb728c7c120888835de3
inga911 <inga.banaityte221@gmail.com>
2023-08-29 20:10:33 +0200

    fetch css

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
-const ulDOM = document.querySelector('ul');
+import { renderUserList } from "./user.js";
 
-const data = fetch('')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        for(const user of data) {
-            console.log(user);
-        }
+
+
+const url = 'https://raw.githubusercontent.com/inga911/fetch/';
+const source = 'main/data/users.json';
+// fetch(url + source)
+//     .then((res) => res.json())
+//     .then(renderUserList);
+//     .catch(console.error);
+//     .finally(() => {
+//     console.log('baigtas darbas');
+// }
+
+const res = await fetch(url + source);
+const data = await res.json();
+renderUserList(data);
+
+
+
+const sourceCSS = 'main/style/style.css';
+fetch(url + sourceCSS)
+    .then(res => res.text())
+    .then(css => {
+        const headDOM = document.querySelector('head');
+        document
+            .querySelector('head')
+            .insertAdjacentHTML('beforeend', `<style>${css}</style`)
     })
